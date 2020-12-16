fdb9c1264aaa0f501a228ee2f58851b8dea3d32e
Denis Krasilnikov <denis.y.krasilnikov@gmail.com>
2020-12-16 16:36:57 +0400

    Fixes Config.java to throw more understandable exception on .env absence

--- src/main/java/com/thecatapi/config/Config.java
+++ src/main/java/com/thecatapi/config/Config.java
@@ -3,9 +3,12 @@ package com.thecatapi.config;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class Config {
-    private static final Dotenv config = Dotenv.load();
+    private static Dotenv config;
 
     public static String get(String key) {
+        if (config == null) {
+            config = Dotenv.load();
+        }
         return config.get(key);
     }
 }
